db6d78d46b4af92b22529dd9c4809f615075d02a
yuns94 <than4502@gmail.com>
2020-04-22 18:18:31 +0900

    event

--- javascript/index.js
+++ javascript/index.js
@@ -19,22 +19,58 @@ console.log(greetYuns);
 
 */
 
-// 정의 명은 중복 불가 , 함수명은 중복 가능.
-const calculator1 = {
+/* 정의 명은 중복 불가 , 함수명은 중복 가능.
+const calculator = {
   plus: function (a, b) {
     return a + b;
   },
-};
-
-//console.log(greetYuns);
-const plus = calculator1.plus(5, 5);
-console.log(plus);
-
-const calculator2 = {
   minus: function (a, b) {
     return a - b;
   },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  squared: function (a, b) {
+    return a ** b;
+  },
 };
 
-const minus = calculator2.minus(30, 10);
-console.log(minus);
+const plus = calculator.plus(5, 5);
+const minus = calculator.minus(20, 10);
+const multiply = calculator.multiply(2, 5);
+const squared = calculator.squared(2, 2);
+
+console.log(plus, minus, multiply, squared);
+
+*/
+const title = document.querySelector("#title");
+
+const superEventHandler = {
+  handleClick: function handleClick() {
+    title.style.color = "orange";
+    title.innerHTML = "The mouse is here!";
+  },
+
+  handleMouseleave: function handleMouseleave() {
+    title.style.color = "blue";
+    title.innerHTML = "The mouse is gone!";
+  },
+
+  windowContextmenu: function windowContextmenu() {
+    title.style.color = "green";
+    title.innerHTML = "That was a right click!";
+  },
+
+  windowResize: function windowResize() {
+    title.style.color = "purple";
+    title.innerHTML = "You just resized!";
+  },
+};
+
+title.addEventListener("mouseenter", superEventHandler.handleClick);
+
+title.addEventListener("mouseleave", superEventHandler.handleMouseleave);
+
+window.addEventListener("contextmenu", superEventHandler.windowContextmenu);
+
+window.addEventListener("resize", superEventHandler.windowResize);
